8dfd493bdf6501ebeec7fd9700f50eb9d6c552ff
eamsdev <76290232+pete-eams@users.noreply.github.com>
2023-08-20 16:06:47 +1000

    added navbar

--- src/lib/components/Navbar.tsx
+++ src/lib/components/Navbar.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import CloseIcon from '@mui/icons-material/Close';
 import MenuIcon from '@mui/icons-material/Menu';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer/Drawer';
 import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -23,6 +25,53 @@ const navItems = [
 ];
 
 export default function NavBar() {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const handleDrawerToggle = () => {
+    setMobileOpen((state) => !state);
+  };
+
+  const drawer = (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap="54px"
+      flexShrink={0}
+    >
+      <Typography variant="h6" component="div" fontWeight={600} fontSize={18}>
+        EAMS@DEV
+      </Typography>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        gap="20px"
+      >
+        {navItems.map((item) => (
+          <Button
+            disableRipple
+            onClick={() => (location.href = item.link)}
+            sx={{
+              fontSize: '20px',
+              fontWeight: '400 !important',
+              color: 'text.primary',
+              textTransform: 'none',
+              ':hover': {
+                backgroundColor: 'background.default',
+              },
+            }}
+            key={item.label}
+          >
+            {item.label}
+          </Button>
+        ))}
+      </Box>
+      <CloseIcon />
+    </Box>
+  );
+
   const toolbarStyles = {
     marginY: '10px',
     flexDirection: {
@@ -40,7 +89,7 @@ export default function NavBar() {
               color="inherit"
               aria-label="open drawer"
               edge="start"
-              onClick={() => {}}
+              onClick={handleDrawerToggle}
               sx={{ display: { sm: 'none' } }}
             >
               <MenuIcon />
@@ -99,6 +148,29 @@ export default function NavBar() {
           />
         </Container>
       </AppBar>
+      <Box component="nav">
+        <Drawer
+          onClick={handleDrawerToggle}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: '100%',
+              justifyContent: 'center',
+              backgroundColor: 'background.default',
+              backgroundImage: 'none',
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
       <Toolbar sx={toolbarStyles} />
     </Box>
   );
