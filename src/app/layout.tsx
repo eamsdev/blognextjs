02c2639884c725e280d02855d15c4bde51b0e1c9
eamsdev <76290232+pete-eams@users.noreply.github.com>
2023-08-21 17:41:23 +1000

    added Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { darkTheme } from '../lib/themes/SiteThemes';
 import './globals.css';
+import * as gtag from './gtag';
 import Footer from '@components/Footer';
 import FullscreenContainer from '@components/FullscreenContainer';
 import Header from '@components/Header';
@@ -7,6 +8,7 @@ import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import type { Metadata } from 'next';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: "Pete Eamsuwan's Software Development Blog",
@@ -17,6 +19,24 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <head>
         <link rel="preload" href="/profilePhoto.webp" as="image" />
       </head>
